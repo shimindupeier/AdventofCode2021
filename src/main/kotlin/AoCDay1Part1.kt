d5373df8c7b92a285973208a35b42e8942f5e46a
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-12-01 18:14:53 +0000

    refactor AoCDay1Part1

--- src/main/kotlin/AoCDay1Part1.kt
+++ src/main/kotlin/AoCDay1Part1.kt
@@ -1,13 +1,7 @@
 object AoCDay1Part1 {
     fun numDepthInc(list: List<Int>) : Int {
-        var currDepth = readLine()?.toInt() ?: 0
-        var nextDept = readLine()?.toInt() ?: 0
         var countInc = 0
-        do {
-            if (nextDept > currDepth) countInc += 1
-            currDepth = nextDept
-            nextDept = readLine()?.toInt() ?: 0
-        } while (nextDept != 0)
+        list.windowed(2, 1, false).map { if (it.first() < it.last()) countInc += 1 }
         return countInc
     }
 }
