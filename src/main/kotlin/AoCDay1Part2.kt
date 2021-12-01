9a04081ebfd739940e95cb841b8e071445f84f5f
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-12-01 18:20:33 +0000

    refactor AoCDay1Part2

--- src/main/kotlin/AoCDay1Part2.kt
+++ src/main/kotlin/AoCDay1Part2.kt
@@ -2,8 +2,9 @@ object AoCDay1Part2 {
     fun numDeptIncP2(list: List<Int>) : Int {
         val windowSize = 3
         var countInc = 0
-        val sumWindowedList = list.windowed(windowSize, 1, false).map { it.sum() }
-        sumWindowedList.windowed(2, 1, false).map { if (it.first() < it.last()) countInc += 1 }
+        list.windowed(windowSize, 1, false).map { it.sum() }
+            .windowed(2, 1, false)
+            .map { if (it.first() < it.last()) countInc += 1 }
         return countInc
     }
 }
