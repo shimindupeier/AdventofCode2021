72e6a837178729e9ebfc96246a647962581b602d
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-12-03 16:46:57 +0000

    initial commit

---
+++ src/main/kotlin/AoCDay3Part1.kt
@@ -0,0 +1,17 @@
+import kotlin.experimental.inv
+
+object AoCDay3Part1 {
+    fun bitManipulation(data: List<String>): Int {
+        val dataInput = data.map { it.split("(?<=\\d)(?=\\d)".toRegex()) }
+
+        var s = ""
+        for (i in 0 until 12) {
+            var (trueCnt, falseCnt) = Pair(0,0)
+            dataInput.forEach {
+                if (it[i] == "1") trueCnt++ else falseCnt++
+            }
+            s += if (trueCnt > falseCnt) "1" else "0"
+        }
+        return (s.toInt(2)).times(s.toInt(2).xor("111111111111".toInt(2)))
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,14 @@
 import java.io.File
 
 fun main() {
+    val list3 = File("C:\\Users\\jasminedupre\\IdeaProjects\\AdventOfCode2021\\src\\main\\resources\\day3Input.txt").readLines().toList()
+    println(AoCDay3Part1.bitManipulation(list3))
+
+//    val list2 = File("C:\\Users\\jasminedupre\\IdeaProjects\\AdventOfCode2021\\src\\main\\resources\\day2Input.txt").readLines().toList()
+//    println(AoCDay2Part2.calFinalPosition(list2))
+
 //    val list = File("C:\\Users\\jasminedupre\\IdeaProjects\\AdventOfCode2021\\src\\main\\resources\\depth measurement.txt").readLines().map { it.toInt() }.toList()
 //    println(AoCDay1Part1.numDepthInc(list))
 //    println(AoCDay1Part2.numDeptIncP2(list))
 
-    val list2 = File("C:\\Users\\jasminedupre\\IdeaProjects\\AdventOfCode2021\\src\\main\\resources\\day2Input.txt").readLines().toList()
-    println(AoCDay2Part2.calFinalPosition(list2))
 }
