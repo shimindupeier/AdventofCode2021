e60dff05d3afd8ea8d63966b5539c6574dfb8069
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-12-03 22:22:41 +0000

    refactored AoCDay3Part2

--- src/main/kotlin/AoCDay3Part2.kt
+++ src/main/kotlin/AoCDay3Part2.kt
@@ -1,20 +1,20 @@
 object AoCDay3Part2 {
     fun calLifeSupportRating(data: List<String>): Int {
         val dataInput = data.map { it.split("(?<=\\d)(?=\\d)".toRegex()) }
-        var (trueCnt, falseCnt) = Pair(0, 0)
+        var (onebit, zeroBit) = Pair(0, 0)
         val size = dataInput[0].size
 
         var oxyGenerator = dataInput
         for (i in 0 until size) {
             oxyGenerator.forEach {
-                if (it[i] == "1") trueCnt++ else falseCnt++
+                if (it[i] == "1") onebit++ else zeroBit++
             }
             oxyGenerator = when  {
-                trueCnt > falseCnt -> oxyGenerator.filter { it[i] == "1" }
-                falseCnt > trueCnt -> oxyGenerator.filter { it[i] == "0" }
+                onebit > zeroBit -> oxyGenerator.filter { it[i] == "1" }
+                zeroBit > onebit -> oxyGenerator.filter { it[i] == "0" }
                 else -> oxyGenerator.filter { it[i] == "1" }
             }
-            trueCnt = 0; falseCnt = 0
+            onebit = 0; zeroBit = 0
             if (oxyGenerator.size == 1) break
         }
         val oxyRating = oxyGenerator.joinToString { it.joinToString("") }.toInt(2)
@@ -22,14 +22,14 @@ object AoCDay3Part2 {
         var carbonGenerator = dataInput
         for (i in 0 until size) {
             carbonGenerator.forEach {
-                if (it[i] == "1") trueCnt++ else falseCnt++
+                if (it[i] == "1") onebit++ else zeroBit++
             }
             carbonGenerator = when  {
-                trueCnt > falseCnt -> carbonGenerator.filter { it[i] == "0" }
-                falseCnt > trueCnt -> carbonGenerator.filter { it[i] == "1" }
+                onebit > zeroBit -> carbonGenerator.filter { it[i] == "0" }
+                zeroBit > onebit -> carbonGenerator.filter { it[i] == "1" }
                 else -> carbonGenerator.filter { it[i] == "0" }
             }
-            trueCnt = 0; falseCnt = 0
+            onebit = 0; zeroBit = 0
             if (carbonGenerator.size == 1) break
         }
 
