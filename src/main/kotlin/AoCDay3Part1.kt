16b0eaca15067db03b950b6bc3a97b82db3f684d
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-12-03 18:22:39 +0000

    refactored AoCDay3Part1

--- src/main/kotlin/AoCDay3Part1.kt
+++ src/main/kotlin/AoCDay3Part1.kt
@@ -1,9 +1,8 @@
 import kotlin.experimental.inv
 
 object AoCDay3Part1 {
-    fun bitManipulation(data: List<String>): Int {
+    fun powerConsumption(data: List<String>): Int {
         val dataInput = data.map { it.split("(?<=\\d)(?=\\d)".toRegex()) }
-
         var s = ""
         for (i in 0 until 12) {
             var (trueCnt, falseCnt) = Pair(0,0)
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ import java.io.File
 
 fun main() {
     val list3 = File("C:\\Users\\jasminedupre\\IdeaProjects\\AdventOfCode2021\\src\\main\\resources\\day3Input.txt").readLines().toList()
-    println(AoCDay3Part1.bitManipulation(list3))
+    println(AoCDay3Part1.powerConsumption(list3))
 
 //    val list2 = File("C:\\Users\\jasminedupre\\IdeaProjects\\AdventOfCode2021\\src\\main\\resources\\day2Input.txt").readLines().toList()
 //    println(AoCDay2Part2.calFinalPosition(list2))
