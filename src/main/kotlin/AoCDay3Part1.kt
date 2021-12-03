497ae67680a548013a5e1d408bc40342a5f65745
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-12-03 21:12:32 +0000

    first complete

--- src/main/kotlin/AoCDay3Part1.kt
+++ src/main/kotlin/AoCDay3Part1.kt
@@ -4,7 +4,7 @@ object AoCDay3Part1 {
     fun powerConsumption(data: List<String>): Int {
         val dataInput = data.map { it.split("(?<=\\d)(?=\\d)".toRegex()) }
         var s = ""
-        for (i in 0 until 12) {
+        for (i in 0 until dataInput[0].size) {
             var (trueCnt, falseCnt) = Pair(0,0)
             dataInput.forEach {
                 if (it[i] == "1") trueCnt++ else falseCnt++
--- src/main/kotlin/AoCDay3Part2.kt
+++ src/main/kotlin/AoCDay3Part2.kt
@@ -1,2 +1,39 @@
 object AoCDay3Part2 {
+    fun calLifeSupportRating(data: List<String>): Int {
+        var dataInput = data.map { it.split("(?<=\\d)(?=\\d)".toRegex()) }
+        var (trueCnt, falseCnt) = Pair(0, 0)
+        val size = dataInput[0].size
+        for (i in 0 until size) {
+            dataInput.forEach {
+                if (it[i] == "1") trueCnt++ else falseCnt++
+            }
+            dataInput = when  {
+                trueCnt > falseCnt -> dataInput.filter { it[i] == "1" }
+                falseCnt > trueCnt -> dataInput.filter { it[i] == "0" }
+                else -> dataInput.filter { it[i] == "1" }
+            }
+            trueCnt = 0; falseCnt = 0
+            if (dataInput.size == 1) break
+        }
+        val oxyRating = dataInput.joinToString { it.joinToString("") }.toInt(2)
+        println(dataInput.size)
+
+        dataInput = data.map { it.split("(?<=\\d)(?=\\d)".toRegex()) }
+        for (i in 0 until size) {
+            dataInput.forEach {
+                if (it[i] == "1") trueCnt++ else falseCnt++
+            }
+            dataInput = when  {
+                trueCnt > falseCnt -> dataInput.filter { it[i] == "0" }
+                falseCnt > trueCnt -> dataInput.filter { it[i] == "1" }
+                else -> dataInput.filter { it[i] == "0" }
+            }
+            trueCnt = 0; falseCnt = 0
+            if (dataInput.size == 1) break
+            //println(dataInput)
+        }
+        val carbonRating = dataInput.joinToString { it.joinToString("") }.toInt(2)
+        println(dataInput.size)
+        return oxyRating.times(carbonRating)
+    }
 }
