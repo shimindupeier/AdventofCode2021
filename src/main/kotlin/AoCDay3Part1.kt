66b797b5d6237931d9f92f7493fdc87df5111b64
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-12-03 22:16:08 +0000

    refactored AoCDay3Part1

--- src/main/kotlin/AoCDay3Part1.kt
+++ src/main/kotlin/AoCDay3Part1.kt
@@ -1,14 +1,18 @@
 object AoCDay3Part1 {
     fun powerConsumption(data: List<String>): Int {
         val dataInput = data.map { it.split("(?<=\\d)(?=\\d)".toRegex()) }
-        var s = ""
+        var gammaBinary = ""
+        var epsilonBinary = ""
         for (i in 0 until dataInput[0].size) {
-            var (trueCnt, falseCnt) = Pair(0,0)
+            var (oneBit, zeroBit) = Pair(0,0)
             dataInput.forEach {
-                if (it[i] == "1") trueCnt++ else falseCnt++
+                if (it[i] == "1") oneBit++ else zeroBit++
             }
-            s += if (trueCnt > falseCnt) "1" else "0"
+            gammaBinary += if (oneBit > zeroBit) "1" else "0"
+            epsilonBinary += if (oneBit > zeroBit) "0" else "1"
         }
-        return (s.toInt(2)).times(s.toInt(2).xor("111111111111".toInt(2)))
+        val gammaRate = gammaBinary.toInt(2)
+        val epsilonRate = epsilonBinary.toInt(2)
+        return (gammaRate).times(epsilonRate)
     }
 }
