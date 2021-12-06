ce8d1f46491919786ed2712e4407f0a5b95a9961
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-12-06 21:27:39 +0000

    Refactored spawnFishes

--- src/main/kotlin/LanternFishCounter.kt
+++ src/main/kotlin/LanternFishCounter.kt
@@ -3,20 +3,18 @@ import java.io.File
 class LanternFishCounter {
     private val days = 18
     private val filename =
-        "C:\\Users\\jasminedupre\\IdeaProjects\\AdventOfCode2021\\src\\main\\resources\\day6Input.txt"
+        "C:\\Users\\jasminedupre\\IdeaProjects\\AdventOfCode2021\\src\\main\\resources\\day6TestSmallSample.txt"
     private val data = File(filename).bufferedReader().readLine()
     private val lanternFirstInitState = data.split("[,]".toRegex()).map { it.toInt() }
 
     fun spawnFishes() {
         val spawningList = lanternFirstInitState.toMutableList()
-
-        for (day in 1 .. days) {
-            if (spawningList.contains(0)) {
-                val newFish = spawningList.count { it == 0 }
-                (1..newFish).forEach { _ -> spawningList.add(9) }
-                spawningList.mapIndexed { index, i -> if (i == 0) spawningList[index] = 7 }
-            }
-            spawningList.mapIndexed { index, i -> spawningList.set(index, i-1) }
+        var numOfNewFish = 0
+        for (day in 1..days) {
+            spawningList.mapIndexed { index, i -> spawningList.set(index, i - 1) }
+            spawningList.mapIndexed { index, i -> if (i == -1) spawningList[index] = 6 }
+            (1..numOfNewFish).forEach { _ -> spawningList.add(8) }
+            numOfNewFish = spawningList.count { it == 0 }
         }
         println(spawningList.size)
     }
